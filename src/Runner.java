3654b18734d32148665e19c49b3c7ce8aa2b85c8
Annie Wang <anniwang@seas.upenn.edu>
2020-05-05 18:00:12 -0700

    fixed bug in main method

--- src/Runner.java
+++ src/Runner.java
@@ -4,7 +4,7 @@ import java.util.Map;
 public class Runner {
 	public static void main(String[] args) {
 		PageRank.loadUserMap();
-		//PageRank.runPageRank();
+		PageRank.runPageRank();
 		Map<Long, User> userMap = PageRank.userMap;
 		
 		//find the 100 users with highest page rank
