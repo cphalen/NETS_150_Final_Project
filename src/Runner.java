82fc65b15d8df20370b38fab514c99a1e4f822d7
Annie Wang <anniwang@seas.upenn.edu>
2020-05-05 17:56:17 -0700

    put in a main method that proccesses the page rank map

---
+++ src/Runner.java
@@ -0,0 +1,35 @@
+import java.util.HashSet;
+import java.util.Map;
+
+public class Runner {
+	public static void main(String[] args) {
+		PageRank.loadUserMap();
+		//PageRank.runPageRank();
+		Map<Long, User> userMap = PageRank.userMap;
+		
+		//find the 100 users with highest page rank
+		HashSet<Long> highPR = new HashSet<>();
+		for (Long pr: userMap.keySet()) {
+			if (highPR.size() < 100) {
+				highPR.add(pr);
+			} else {
+				for (Long val: highPR) {
+					if (pr > val) {
+						highPR.remove(val);
+						highPR.add(pr);
+						break;
+					}
+				}
+			}
+		}
+		
+		//find the percentage of verified users in those 100 users
+		int count = 0;
+		for (Long pr: highPR) {
+			if (TwitterQuery.isUserVerified(userMap.get(pr))) {
+				count ++;
+			}
+		}
+		System.out.println("Percentage of Verified Users in top 100: " + (double)count/100);
+	}
+}
